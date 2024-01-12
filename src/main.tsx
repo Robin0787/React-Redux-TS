9236030e1a9e2b672424ef7aa3a0085311fac91e
Robin <mohammadrobin636@gmail.com>
2024-01-12 17:27:21 +0600

    'Updated'

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import "./index.css";
 import Home from "./pages/Home/Home.tsx";
+import { store } from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <Provider store={store}>
+      <Home />
+    </Provider>
     <Toaster position="bottom-left" />
   </React.StrictMode>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,45 @@
-import Todo from "../../components/ui/Todo/Todo";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  decrementByValue,
+  increment,
+  incrementByValue,
+} from "../../redux/features/counterSlice";
 
 const Home = () => {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="bg-black h-screen w-full text-white center overflow-hidden">
-      <Todo />
+      <div className="border border-white/50 p-8 rounded-md">
+        <h1 className="text-center text-4xl text-white py-4 bg-white/30 rounded-lg">
+          {count}
+        </h1>
+        <div className="flex justify-center items-center gap-3 mt-8">
+          <div className="flex flex-col justify-between items-center gap-3 w-1/2 mx-auto">
+            <button className="btn" onClick={() => dispatch(increment())}>
+              Increment
+            </button>
+            <button
+              className="btn"
+              onClick={() => dispatch(incrementByValue(5))}
+            >
+              Increment By 5
+            </button>
+          </div>
+          <div className="flex flex-col justify-between items-center gap-3 w-1/2">
+            <button className="btn" onClick={() => dispatch(decrement())}>
+              Decrement
+            </button>
+            <button
+              className="btn"
+              onClick={() => dispatch(decrementByValue(5))}
+            >
+              Decrement By 5
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
